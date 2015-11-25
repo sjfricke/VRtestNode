a4b92cf3f621baaa21030989255c0000d8ffab16
Spencer <spencer@branch2.com>
2015-11-25 00:03:20 -0600

    lets go

---
+++ server.js
@@ -0,0 +1,62 @@
+// server.js
+
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+var color = "red";
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ color: color});   
+});
+
+router.post('/green', function(req, res) {
+    color = "green";
+    consol.log("green");
+    res.json({color: color});
+});
+
+router.post('/red', function(req, res) {
+    color = "red";
+    consol.log("red");
+    res.json({color: color});
+});
+
+router.post('/blue', function(req, res) {
+    color = "blue";
+    consol.log("blue");
+    res.json({color: color});
+});
+
+router.post('/red', function(req, res) {
+    color = "yellow";
+    consol.log("yellow");
+    res.json({color: color});
+});
+
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
